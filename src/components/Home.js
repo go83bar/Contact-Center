b940f64ca9c98d01e67a0bc64711bbded9f539c6
Mike Cernik <cernik@go83bar.com>
2020-04-13 09:09:45 -0500

    Added home content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,82 @@
 import React, {Component} from 'react'
+import {MDBCardBody, MDBCard, MDBCardText, MDBContainer, MDBCol, MDBRow, MDBIcon, MDBBtn} from "mdbreact";
+import {Link} from "react-router-dom";
+import {connect} from "react-redux";
 
 class Home extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-        };
+
+        this.logout=this.logout.bind(this)
+        this.state = {};
+    }
+
+    logout(){
+        this.props.dispatch({type: 'LOG_OUT_USER', payload: {}})
     }
 
     render() {
         return (
-            <div>Home</div>
+            <MDBContainer>
+                <MDBRow style={{textAlign: "center", marginTop: "20%"}}>
+                    <MDBCol md='4' className={"offset-md-2"}>
+                        <MDBCard style={{width: "100%", height: "15rem"}}>
+                            <MDBCardBody style={{marginTop: "50px"}}>
+                                <MDBCardText style={{fontSize: "20px"}}>
+                                    <Link to="/interaction"><MDBBtn floating><MDBIcon icon={"headphones"} size={"2x"}/></MDBBtn><br/>FETCH NEW LEAD</Link>
+
+                                </MDBCardText>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                    <MDBCol md='4'>
+                        <MDBCard style={{width: "100%", height: "15rem"}}>
+                            <MDBCardBody style={{marginTop: "50px"}}>
+                                <MDBCardText style={{fontSize: "20px"}}>
+                                <Link to="/search"><MDBBtn floating><MDBIcon icon={"search"}
+                                                                             size={"2x"}/></MDBBtn><br/>FIND A LEAD</Link>
+                                </MDBCardText>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                </MDBRow>
+                <MDBRow style={{textAlign: "center", marginTop: "30px"}}>
+
+                    <MDBCol md='4' className={"offset-md-2"}>
+                        <MDBCard style={{width: "100%", height: "15rem"}}>
+                            <MDBCardBody style={{marginTop: "50px"}}>
+                                <MDBCardText style={{fontSize: "20px"}}>
+                                <Link to="/recent" ><MDBBtn floating><MDBIcon icon={"list"} size={"2x"}/></MDBBtn><br/>RECENT LEADS</Link>
+                                </MDBCardText>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                    <MDBCol md='4'>
+                        <MDBCard style={{width: "100%", height: "15rem"}}>
+                            <MDBCardBody style={{marginTop: "50px"}}>
+                                <MDBCardText style={{fontSize: "20px"}}>
+                                    <Link to="#" onClick={this.logout}><MDBBtn floating onClick={this.logout}><MDBIcon icon={"lock"} size={"2x"}/></MDBBtn><br/>SIGN OUT</Link>
+                                </MDBCardText>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+
+                </MDBRow>
+            </MDBContainer>
         )
     }
 }
-export default Home;
+
+const mapStateToProps = state => {
+    return {
+        auth: state.auth,
+        localization: state.localization
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {dispatch}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/components/Login.js
+++ src/components/Login.js
@@ -42,7 +42,7 @@ class Login extends Component {
                 <MDBRotatingCard flipped={this.state.flipped} className="text-center h-100 w-100" style={{margin: "0 auto", marginTop: "20%", width: "500px"}}>
                     <MDBCard className="face front" narrow >
                         <MDBCardImage
-                            className='view view-cascade gradient-card-header blue-gradient'
+                            className='view view-cascade gradient-card-header peach-gradient'
                             cascade
                             tag='div'
                         >
@@ -51,13 +51,13 @@ class Login extends Component {
                             <h2 className='h2-responsive' style={{marginTop: "10px"}}>{localization.title}</h2>
                         </MDBCardImage>
                         <MDBCardBody cascade className='text-center'>
-                            <div className={"text-left"}><MDBInput label={localization.frontPlaceholder} outline icon="mobile" className={"text-left"}/></div>
-                            <MDBBtn gradient="blue" rounded onClick={this.getPin}><h5 style={{marginBottom:"0px"}}> {localization.frontButton} <MDBIcon icon="angle-double-right" style={{marginLeft : "10px"}}/> </h5></MDBBtn>
+                            <div className={"text-left"}><MDBInput label={localization.frontPlaceholder} outline icon="mobile-alt" className={"text-left"}/></div>
+                            <MDBBtn gradient="peach" rounded onClick={this.getPin}><h5 style={{marginBottom:"0px"}}> {localization.frontButton} <MDBIcon icon="angle-double-right" style={{marginLeft : "10px"}}/> </h5></MDBBtn>
                         </MDBCardBody>
                     </MDBCard>
                     <MDBCard className="face back" narrow >
                         <MDBCardImage
-                            className='view view-cascade gradient-card-header blue-gradient'
+                            className='view view-cascade gradient-card-header peach-gradient'
                             cascade
                             tag='div'
                         >
@@ -67,7 +67,7 @@ class Login extends Component {
                         </MDBCardImage>
                         <MDBCardBody cascade className='text-center'>
                             <div className={"text-left"}><MDBInput label={localization.backPlaceholder} outline icon="lock" className={"text-left"}/></div>
-                            <MDBBtn gradient="blue" rounded onClick={this.login}><h5 style={{marginBottom:"0px"}}> <MDBIcon icon="unlock" style={{marginRight : "10px"}}/> {localization.backButton} </h5></MDBBtn>
+                            <MDBBtn gradient="peach" rounded onClick={this.login}><h5 style={{marginBottom:"0px"}}> <MDBIcon icon="unlock" style={{marginRight : "10px"}}/> {localization.backButton} </h5></MDBBtn>
                         </MDBCardBody>
                     </MDBCard>
                 </MDBRotatingCard>
