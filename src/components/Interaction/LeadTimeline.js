01b16b287f8448c78e0c36f14c0a2926b933e18c
Mike Cernik <cernik@go83bar.com>
2020-04-13 19:41:20 -0500

    Lead Stuff

--- src/components/Interaction/LeadTimeline.js
+++ src/components/Interaction/LeadTimeline.js
@@ -1,36 +1,102 @@
 import React, {Component} from 'react'
-import {MDBContainer, MDBIcon, MDBStepper, MDBStep, MDBBtn} from "mdbreact";
+import {
+    MDBContainer,
+    MDBStepper,
+    MDBStep,
+    MDBCardHeader,
+    MDBCardBody,
+    MDBCard,
+    MDBRow,
+    MDBCol
+} from "mdbreact";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCalendarCheck, faComment, faEnvelope, faPhone, faPoll} from '@fortawesome/pro-solid-svg-icons'
+import {faCircle} from '@fortawesome/pro-light-svg-icons'
 
 class LeadTimeline extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-        };
+        this.state = {};
     }
 
     render() {
         return (
             <MDBContainer>
-                <MDBStepper vertical>
-                    <MDBStep>
-                        <MDBBtn floating outline><MDBIcon icon={"calendar-check"} className={"timelineIcon"}/></MDBBtn>
-                    </MDBStep>
-                    <MDBStep>
-                        <MDBBtn floating outline><MDBIcon icon={"comment"} className={"timelineIcon"}/></MDBBtn>
-                    </MDBStep>
-                    <MDBStep>
-                        <MDBBtn floating outline><MDBIcon icon={"envelope"} className={"timelineIcon"}/></MDBBtn>
-                    </MDBStep>
-                    <MDBStep>
-                        <MDBBtn floating outline><MDBIcon icon={"phone"} className={"timelineIcon"}/></MDBBtn>
-                    </MDBStep>
-                    <MDBStep>
-                        <MDBBtn floating outline><MDBIcon icon={"poll"} className={"timelineIcon"} rotate={"90"}/></MDBBtn>
-                    </MDBStep>
-                </MDBStepper>
+                <MDBRow>
+                    <MDBCol size={"4"}>
+                        <MDBCard className="mb-3">
+                            <MDBCardHeader className={"card-header-no-back-no-border"}>Touchpoints</MDBCardHeader>
+                            <MDBCardBody>
+                                <MDBStepper vertical>
+                                    <MDBStep>
+                        <span className="fa-layers fa-fw fa-4x">
+                            <FontAwesomeIcon icon={faCircle} className={"darkIcon"}/>
+                            <FontAwesomeIcon icon={faCalendarCheck} transform={"shrink-8"} className={"darkIcon"}/>
+                            <span className="fa-layers-counter fa-layers-top-right darkBack">3</span>
+                        </span>
+                                        <div style={{minHeight: "50px"}}
+                                             className={"step-content step-content-top"}>Test
+                                        </div>
+                                    </MDBStep>
+                                    <MDBStep>
+                        <span className="fa-layers fa-fw fa-4x">
+                            <FontAwesomeIcon icon={faCircle} className={"darkIcon"}/>
+                            <FontAwesomeIcon icon={faComment} transform={"shrink-8"} className={"dark"}/>
+                            <span className="fa-layers-counter fa-layers-top-right"
+                                  style={{background: "Tomato"}}>3</span>
+                        </span>
+                                        <div style={{minHeight: "50px"}}
+                                             className={"step-content step-content-top"}>Test
+                                        </div>
+                                    </MDBStep>
+                                    <MDBStep>
+                        <span className="fa-layers fa-fw fa-4x">
+                            <FontAwesomeIcon icon={faCircle} className={"darkIcon"}/>
+                            <FontAwesomeIcon icon={faEnvelope} transform={"shrink-8"} className={"dark"}/>
+                            <span className="fa-layers-counter fa-layers-top-right"
+                                  style={{background: "Tomato"}}>3</span>
+                        </span>
+                                        <div style={{minHeight: "50px"}}
+                                             className={"step-content step-content-top"}>Test
+                                        </div>
+                                    </MDBStep>
+                                    <MDBStep>
+                        <span className="fa-layers fa-fw fa-4x">
+                            <FontAwesomeIcon icon={faCircle} className={"darkIcon"}/>
+                            <FontAwesomeIcon icon={faPhone} transform={"shrink-8"} className={"dark"}/>
+                            <span className="fa-layers-counter fa-layers-top-right"
+                                  style={{background: "Tomato"}}>3</span>
+                        </span>
+                                        <div style={{minHeight: "50px"}}
+                                             className={"step-content step-content-top"}>Test
+                                        </div>
+                                    </MDBStep>
+                                    <MDBStep>
+                        <span className="fa-layers fa-fw fa-4x">
+                            <FontAwesomeIcon icon={faCircle} className={"darkIcon"}/>
+                            <FontAwesomeIcon icon={faPoll} transform={"shrink-8"} rotation={90} className={"darkIcon"}/>
+                            <span className="fa-layers-counter fa-layers-top-right"
+                                  style={{background: "Tomato"}}>3</span>
+                        </span>
+                                        <div style={{minHeight: "50px"}}
+                                             className={"step-content step-content-top"}>Test
+                                        </div>
+                                    </MDBStep>
+                                </MDBStepper>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                    <MDBCol size={"8"}>
+                    <MDBCard>
+                        <MDBCardHeader className={"card-header-no-back-no-border"}>All</MDBCardHeader>
+                        <MDBCardBody></MDBCardBody>
+                    </MDBCard>
+                    </MDBCol>
+                 </MDBRow>
             </MDBContainer>
         )
     }
 }
+
 export default LeadTimeline;
